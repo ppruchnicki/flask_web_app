0ae5743b30f158c7cef8b49e787860d25022e1f7
GrandeDonPablo <pawelek.poland@gmail.com>
2020-06-20 17:41:31 +0200

    Added navbar, main.html, login page - no functionality, custom css, inherited html

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello? <h1>HELLO world<h1>"
+    return render_template("main.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
