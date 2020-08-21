8e7ccdd1410f4c616f7bb769edfa74549c8213b7
GrandeDonPablo <pawelek.poland@gmail.com>
2020-08-21 16:38:54 +0200

    implemented pythond decouple and env variables

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
+from decouple import config
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = config('SECRET_KEY') #'+=uo_)pczyks9(3dtpg2kgn-%g08=v'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
-    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
-    MAIL_SERVER = 'smtp.googlemail.com'
+    SECURITY_PASSWORD_SALT = config('SECURITY_PASSWORD_SALT') #'basdhHUIASDgvmsda38jn'
+    MAIL_SERVER = config('MAIL_SERVER')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USER')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = config('MAIL_USER') #'pp3148212@gmail.com'
+    MAIL_PASSWORD = config('MAIL_PASSWORD') #'beTk7VQnFHKr3j3'
     FLASK_ADMIN_SWATCH = 'yeti'
