978f74f9bbbd8f07eebe5930e19fe899cfb748ce
GrandeDonPablo <pawelek.poland@gmail.com>
2020-08-21 16:43:36 +0200

    small fixes

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ from decouple import config
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = config('SECRET_KEY') #'+=uo_)pczyks9(3dtpg2kgn-%g08=v'
+    SECRET_KEY = config('SECRET_KEY') 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
-    SECURITY_PASSWORD_SALT = config('SECURITY_PASSWORD_SALT') #'basdhHUIASDgvmsda38jn'
+    SECURITY_PASSWORD_SALT = config('SECURITY_PASSWORD_SALT')
     MAIL_SERVER = config('MAIL_SERVER')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = config('MAIL_USER') #'pp3148212@gmail.com'
-    MAIL_PASSWORD = config('MAIL_PASSWORD') #'beTk7VQnFHKr3j3'
+    MAIL_USERNAME = config('MAIL_USER') 
+    MAIL_PASSWORD = config('MAIL_PASSWORD') 
     FLASK_ADMIN_SWATCH = 'yeti'
